a6eb86afb3271b31e25827822d1824645d19ad1e
ngarciadanila <143655749+ngarciadanila@users.noreply.github.com>
2023-09-15 16:50:15 -0400

    Add major column

--- lab-01/Names.java
+++ lab-01/Names.java
@@ -6,9 +6,9 @@
 public class Names {
   public static void main(String[] args) {
     System.out.println();
-    System.out.println(" \tName\t\tHometown") ;
-    System.out.println("\t====\t\t========");
-    System.out.println("\tSally\t\tRoanoke");
+    System.out.println(" \tName\t\tHometown\t\tMajor") ;
+    System.out.println("\t====\t\t========\t\t=====");
+    System.out.println("\tSally\t\tRoanoke\t\tComputer Science");
     System.out.println("\tAlexander\tWashington");
     System.out.println("\tNannette\tHartford");
    System.out.println("\tAlyssa\tHartford")
